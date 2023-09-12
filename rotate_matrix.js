1971895bad447fe5e503b8780d159dd7d21b82e4
Bhuwan Bansal <bhuwan.bansal@outlook.com>
2023-09-12 20:16:20 +1000

    Rotate Matrix JS

--- rotate_matrix.js
+++ rotate_matrix.js
@@ -1,2 +1,12 @@
 /** 
  * @param {number[][]}
+ * @return {void} Do not return anything, modify matrix in-place instead
+ * */ 
+var rotate = function(matrix) {
+// Swap column with rows and find a pattern for either the row or column and use temp values in order 	
+};
+
+
+let matrix = [[1,2,3],[4,5,6],[7,8,9]];
+
+console.log(rotate(matrix));
