9c062313336326fbebce1c8f723ca95eba79ab6d
Bhuwan Bansal <bhuwan.bansal@outlook.com>
2023-09-20 13:34:52 +1000

    Inorder Binary Tree Traversal

--- Data Structures/Trees/inorderTraversal.js
+++ Data Structures/Trees/inorderTraversal.js
@@ -10,6 +10,28 @@ class Node {
 
 const inOrderTraversal = (root) => {
 
+    if(root === null) return null;
+
+    let return_arr = []; 
+
+    let stack = [];
+
+    let current = root;
+
+    while(stack.length > 0 || current !== null){
+        if(current !== null){
+            stack.push(current);
+            current = current.left; 
+        }
+        else{
+            current = stack.pop();
+            return_arr.push(current.val); 
+            current = current.right;
+        }
+        
+    }
+    return return_arr;
+
 };
 
 
@@ -20,9 +42,23 @@ const c = new Node('c');
 const d = new Node('d');
 const e = new Node('e');
 const f = new Node('f');
+const h = new Node('h');
 
-a.right = c; 
+/*
+ *                 a
+                  / \
+                  b  c
+                 /\  /\
+                d e  h f
+
+ */
+
+
 a.left = b; 
+a.right = c; 
 b.left = d; 
 b.right = e; 
 c.right = f;
+c.left = h;
+
+console.log(inOrderTraversal(a));
