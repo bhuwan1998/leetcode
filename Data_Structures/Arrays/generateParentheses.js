6e0b9083285795050530d38412e156fd5299bcc1
Bhuwan Bansal <bhuwan.bansal@outlook.com>
2023-12-24 20:23:39 +0800

    Generate Parentheses understood mostly

--- Data_Structures/Arrays/generateParentheses.js
+++ Data_Structures/Arrays/generateParentheses.js
@@ -2,12 +2,57 @@
 
 /*
 * Example 1: 
-*   Input: n = 3
-*    Output: ["((()))","(()())","(())()","()(())","()()()"]
-*/
+* Input: n = 3
+* Output: ["((()))","(()())","(())()","()(())","()()()"]
 
-/*
+
+  Example 3: 
+  Input: n = 2 
+  Output: ["(())", "()()"]
+  
+
+*
 * Example 2:
 * Input: n = 1 
 * Output: ["()"] 
 */
+
+
+/**
+ * @param {number} n 
+ * @return {string[]}
+ */
+
+var generateParentheses = function(n) {
+    // only add open parentheses if open < n 
+    // only add closing parentheses if closed < open 
+    // valid IFF open === closed === n 
+
+    // list of valid 
+    let ans = [];
+    // backtracking => DFS 
+    let stack = [];  
+
+    var backtrack = function(openN, closedN) {
+        if(openN === closedN & openN === n & closedN === n){
+            ans.push("" + stack.join(""));
+            return; 
+        }
+        if(openN < n){
+            stack.push("("); 
+            backtrack(openN+1, closedN);
+            stack.pop();
+        }
+        if(openN > closedN){
+            stack.push(")"); 
+            backtrack(openN, closedN + 1);
+            stack.pop();
+        }
+    }
+
+    backtrack(0, 0)
+    return ans;
+}
+
+
+console.log("Parentheses: ", generateParentheses(3));
