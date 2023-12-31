5f6172f4cf83ac5c23724641f2cf84b2c2cab40a
Bhuwan Bansal <bhuwan.bansal@outlook.com>
2023-12-31 13:02:27 +0800

    Working through some leetcode questions

--- Data_Structures/Stack/carFleet.js
+++ Data_Structures/Stack/carFleet.js
@@ -7,29 +7,29 @@
 
 var carFleet = function(target, position, speed) {
 
-    const cars = position.map((pos, idx) => [pos, speed[idx]] );
-
-    let stack = [] ;
-
-    let fleetCount = 0; 
+    let cars = position.map((pos, idx) => [pos, speed[idx]] );
 
     cars.sort((a,b) => b[0] - a[0]); // sort cars by position descending order 
 
     let timeToReachTarget = cars.map(([pos, spd]) => (target - pos) / spd);
 
+    let stack = [] 
+
+    let fleetCount = 0;
+
     
     for(let time of timeToReachTarget) {
-        while(stack.length > 0 && time >= stack[stack.length - 1]) {
-            stack.pop(); // if a car behind reaches sooner, pop the car in front 
+        while(stack.length == 0 || time > stack[stack.length - 1]) {
+            stack.push(time); // if a car behind reaches sooner, pop the car in front 
+            fleetCount++;
         } 
-
+        
     }
-
-    
+    return fleetCount;
 };
 
 let target = 12
 let position = [10,8,0,5,3]
 let speed = [2,4,1,1,3]
 
-console.log(carFleet(target, position, speed))
+console.log(carFleet(target, position, speed)) 
