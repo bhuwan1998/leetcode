86d48765eff683a88105e9a2bded510159815345
Bhuwan Bansal <bhuwan.bansal@outlook.com>
2024-01-09 19:56:20 +0800

    Last Stack Question to be done tomorrow

---
+++ Data_Structures/Stack/largestRectangeHistogram.js
@@ -0,0 +1,125 @@
+
+
+/*
+
+84. Largest Rectangle in Histogram
+
+Given an array of integers heights representing the histogram's bar height where the width of 
+each bar is 1, return the area of the largest rectangle in the histogram.
+
+ 
+
+Example 1:
+
+
+Input: heights = [2,1,5,6,2,3]
+Output: 10
+Explanation: The above is a histogram where width of each bar is 1.
+The largest rectangle is shown in the red area, which has an area = 10 units.
+Example 2:
+
+
+Input: heights = [2,4]
+Output: 4
+ 
+
+Constraints:
+
+1 <= heights.length <= 105
+0 <= heights[i] <= 104
+*/ 
+
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var largestRectangleArea = function(heights) {
+  
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Read more
+
+
+
+
+
+
+
+
+
+
+Read more
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Read more
+
+
+
+
+Copyright ©️ 2024 LeetCode All rights reserved
+
+16.5K
+
+
+85
+
+
+
+JavaScript
+Auto
+
+
+
+
+1234567
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var largestRectangleArea = function(heights) {
+    
+};
+Saved to local
+Case 1
+Case 2
+
+heights =
+[2,1,5,6,2,3]
+1
+[2,1,5,6,2,3]
+[2,4]
--- Rough/rough.js
+++ Rough/rough.js
@@ -1,6 +1,17 @@
-let array =  [[1,2,3],[4,5,6],[7,8,9]];
-for(let i = 0; i < array.length; i++) {
-    console.log("Space between rows")
-    for(let j = 0; j < array[i].length; j++)
-    console.log(array[i][j]);
-}
+// let array =  [[1,2,3],[4,5,6],[7,8,9]];
+// for(let i = 0; i < array.length; i++) {
+//     console.log("Space between rows")
+//     for(let j = 0; j < array[i].length; j++)
+//     console.log(array[i][j]);
+// }
+
+let position = [10, 8, 0, 5, 3]
+let speed = [2, 4, 1, 1, 3]
+
+let cars = position.map((pos, idx) => [pos, speed[idx]]);
+
+console.log("Before Sorting: " + cars);
+
+cars.sort((a, b) => b[0] - a[0]); // descending order 
+
+console.log("After Sorting: " + cars);
