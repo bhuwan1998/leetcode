d4f3a15afbf1380f583c214bd258041261333966
Bhuwan Bansal <bhuwan.bansal@outlook.com>
2024-04-01 17:22:03 +1030

    Singly Linked List Reversed: Understood

--- Data_Structures/Linked_Lists/reverse_linked_list.js
+++ Data_Structures/Linked_Lists/reverse_linked_list.js
@@ -1,14 +1,12 @@
-class Node {
-    constructor(val){
-        this.val = val; 
-        this.next = null;
-    }
+// Definition of singly-linked list
+function ListNode(val, next) {
+  this.val = val === undefined ? null : val;
+  this.next = next === undefined ? null : next;
 }
 
-
 // const reverseLinkedList = (head) => {
-//     let current = head; 
-//     let prev = null; // current previous value  
+//     let current = head;
+//     let prev = null; // current previous value
 //     while(current !== null){
 //         const next = current.next;
 //         current.next = prev;
@@ -19,32 +17,68 @@ class Node {
 //     return prev;
 // }
 
-const reverseLinkedList = (head, prev = null) => {
-    if(head === null) return prev; 
-    const next = head.next; 
-    head.next = prev; 
-    return reverseLinkedList(next, head);
+// const reverseLinkedList = (head, prev = null) => {
+//   if (head === null) return prev;
+//   const next = head.next;
+//   head.next = prev;
+//   return reverseLinkedList(next, head);
+// };
 
-}
+// const a = new Node("A");
+// const b = new Node("B");
+// const c = new Node("C");
+// const d = new Node("D");
 
+// a.next = b;
+// b.next = c;
+// c.next = d;
 
-const a = new Node('A');
-const b = new Node('B');
-const c = new Node('C');
-const d = new Node('D');
+// reverseLinkedList(a);
 
-a.next = b; 
-b.next = c; 
-c.next = d; 
+// let current = reverseLinkedList(a);
 
+// console.log(reverseLinkedList(d));
 
-reverseLinkedList(a); 
+// while (current !== null) {
+//   console.log(current.val, "->");
+//   current = current.next;
+// }
 
-let current = reverseLinkedList(a);
+/*
+ * @param {ListNode}
+ * @return {ListNode}
+ */
 
-console.log(reverseLinkedList(d));
+var reverseList = function (head) {
+  var current = head;
+  var previous = null;
+  // move to the next until current is null so that we can make changes to the pointers
+  // until last value
+  while (current !== null) {
+    let next = current.next;
+    current.next = previous;
+    previous = current;
+    current = next;
+  }
 
-while(current !== null){
-    console.log(current.val, '->');
-    current = current.next;
+  return previous;
+};
+
+var firstNode = new ListNode(1, null);
+var secondNode = new ListNode(2, null);
+var thirdtNode = new ListNode(3, null);
+var fourthNode = new ListNode(4, null);
+var fifthtNode = new ListNode(5, null);
+
+firstNode.next = secondNode;
+secondNode.next = thirdtNode;
+thirdtNode.next = fourthNode;
+fourthNode.next = fifthtNode;
+
+let reversedListHead = reverseList(firstNode);
+
+let current = reversedListHead;
+while (current !== null) {
+  console.log(current.val);
+  current = current.next;
 }
