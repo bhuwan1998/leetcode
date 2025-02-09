bf6b42b374221b1fb47f0982cb5ab4b74ae748d6
Bhuwan Bansal <bhuwan.bansal@outlook.com>
2025-02-09 18:22:57 +1030

    Added Contains Duplicate

--- Data_Structures/Arrays/containsDuplicate.js
+++ Data_Structures/Arrays/containsDuplicate.js
@@ -1,24 +1,20 @@
 var containsDuplicate = function(nums) {
-    let freq_count = new Map()
-    let counter = 0
-    for(let i = 0; i< nums.length; i++){
-        freq_count.set(nums[i], counter)
-    }
-    for(let i = 0; i< nums.length; i++){
-        if(freq_count.has(nums[i])){
-            let val = freq_count.get(nums[i])
-            freq_count.set(nums[i], val+1)
-            if(freq_count.get(nums[i]) > 1){
-                return true
-            }
+    
+    
+    const numsMap = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        if (numsMap.get(nums[i])) {
+            console.log(numsMap.get(nums[i]));
+            return true;
         }
+        numsMap.set(nums[i], 1);
     }
 
-    console.log(freq_count)
-    return false
+    return false;
 
 };
 
 
-let nums = [1,2,3]
+let nums = [1,2,3,3]
 console.log(containsDuplicate(nums))
